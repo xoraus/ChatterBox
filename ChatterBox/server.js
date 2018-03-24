b3e19f0bcbff25c2ceba062fb0ba1c5acad5948c
Sajjad Salaria <imsafay@gmail.com>
2018-03-24 15:07:17 +0530

    updated

--- ChatterBox/server.js
+++ ChatterBox/server.js
@@ -1,8 +1,27 @@
 var express = require("express")
-
+var bodyParser = require('body-parser')
 var app = express()
+
 app.use(express.static(__dirname))
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: false}))
 
-app.listen(3010)
+var messages = [
 
-console.log("i am listening")
+    
+]
+
+app.get('/messages',(req,res)=>{
+    res.send(messages)
+})
+
+app.post('/messages',(req, res)=>{
+    messages.push(req.body)
+    res.sendStatus(200)
+})
+
+
+var server = app.listen(3010, () => {
+    console.log("Server is listening on port ",server.address().port)
+
+})

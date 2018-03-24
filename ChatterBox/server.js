b80e430a88df5b88263e0483a2b85101ab59b5a1
Sajjad Salaria <imsafay@gmail.com>
2018-03-24 16:57:40 +0530

    removed mt password

--- ChatterBox/server.js
+++ ChatterBox/server.js
@@ -1,27 +1,53 @@
 var express = require("express")
 var bodyParser = require('body-parser')
 var app = express()
+var http = require('http').Server(app)
+var io = require('socket.io')(http)
+var mongoose = require('mongoose')
+
 
 app.use(express.static(__dirname))
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended: false}))
 
-var messages = [
+var dbUrl = 'mongodb://<admin>:<YOURPASSWORDHERE@ds121349.mlab.com:21349/mongo-node'
 
-    
-]
+
+var Message = mongoose.model('Message',{
+
+    name : String,
+    message: String
+})
 
 app.get('/messages',(req,res)=>{
-    res.send(messages)
+    Message.find({}, (err,messages)=>{
+        res.send(messages)
+
+    })
+    
 })
 
 app.post('/messages',(req, res)=>{
-    messages.push(req.body)
-    res.sendStatus(200)
+    var message = new Message(req.body)
+    message.save((err)=>{
+        if(err)
+            sendStatus(500)
+
+        io.emit('message', req.body)
+        res.sendStatus(200)
+    })
+    
 })
 
+io.on('connection',(socket)=> {
+    console.log('user connected')
+})
 
-var server = app.listen(3010, () => {
+mongoose.connect(dbUrl, (err)=>{
+    console.log('mongodb connection successful')
+})
+
+var server = http.listen(3010, () => {
     console.log("Server is listening on port ",server.address().port)
 
 })
